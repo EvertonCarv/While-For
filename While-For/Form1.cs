e7270443f565d310b9db20a3ffe020b25485f338
Everton carvalho <everton.de.carvalho@hotmail.com.br>
2021-11-10 10:58:29 -0300

    Adicionar arquivos de projeto.

---
+++ While-For/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace While_For
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int contador;
+            for (contador = 2021; contador >= 1950; contador--)
+            {
+                cboAno.Items.Add(contador);
+            }
+            /*Declaração da variável contador do tipo inteiro
+             * Laço For - Atribuído o valor de 2021 (ano), até que o ano seja maior ou igual a 1950,
+             * o contador vai fazendo um decremento.
+             * Ou seja começa com 2021 e vai até 1950.
+             * Em seguida os itens são adicionados no Contador.
+             * */
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 novo = new Form2();
+            //Declara o objeto novo tipo form e chama em seguida através do método Show.
+            novo.Show();
+            //fecha o formulário atual
+            this.Visible = false;
+        }
+    }
+}
---
+++ While-For/Form2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace While_For
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 novo = new Form1();
+            //Apresenta o formulário e chama em seguida
+            novo.Show();
+            //Fecha o formulário atual
+            this.Visible = false;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            int contador;
+            contador = 2021;
+
+            while (contador >= 1950)
+            {
+                cboAno.Items.Add(contador);
+                contador--;
+            }
+            /*Declaração da variável contador do tipo inteiro, onde foi atribuído o valor de 2021 (ano)
+             *Enquanto o contador for maior ou igual a 1950, o contador faz o decremento.
+             *Ou seja começa em 2021 e vai até 1950.
+             *Itens são adicionados no Contador.*/
+        }
+    }
+}
